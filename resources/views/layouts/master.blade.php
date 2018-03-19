48962288fac136ae18f0b810ffd6acbdafd942ce
binarywoo <binarywoo@gmail.com>
2018-03-19 14:49:59 +0900

    테마 기본 소스 추가 / 구글 애널리틱스 추가

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
-<html lang="ko">
+<html lang="en">
 	<head>
 		<meta charset="utf-8" />
 		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 		<title>binaryoo</title>
 		
-		<link rel="stylesheet" type="text/css" href="/bootstrap/css/bootstrap.min.css" >
+		<link rel="stylesheet" type="text/css" href="/css/bootstrap.min.css" >
 		<link rel="stylesheet" type="text/css" href="/css/linea.css" >
 		<link rel="stylesheet" type="text/css" href="/css/ionicons.min.css" >
 		<link rel="stylesheet" type="text/css" href="/css/owl.carousel.css" >
@@ -24,5 +24,14 @@
 		<script type="text/javascript" src="/js/script.js"></script>
 		@section('script')
 		@show
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-115949084-1"></script>
+		<script>
+		  window.dataLayer = window.dataLayer || [];
+		  function gtag(){dataLayer.push(arguments);}
+		  gtag('js', new Date());
+
+		  gtag('config', 'UA-115949084-1');
+		</script>
 	</body>
 </html>
